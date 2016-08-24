c238086a0a64e5e270370255ea69445376bfc24d
Phua Kuan Yu <Phuaky@virus.local>
2016-08-24 17:33:53 +0800

    message

--- bomb.js
+++ bomb.js
@@ -1,6 +1,66 @@
-console.log("javascript running");
+var player = 0;
+var wires = [0,0,0,0,0];
+// Game logic
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("DOM loaded");
-});
+// DOM manipulation
+function fiveRandomNumbers() {
+  for (var i = 0; i < wires.length; i++) {
+    wires[i] = Math.random();
+  }
+};
 
+fiveRandomNumbers();
+
+var imgs = document.getElementsByTagName("img");
+for (var i = 0; i < imgs.length; i++) {
+  var img = imgs[i];
+  img.addEventListener("click", changeWire());
+
+}
+function changeWire(ev) {
+  var id = img.id;
+
+  if (id === "Blue") {
+    img.src = "img/cut-blue-wire.png";
+    if (wires[0] > 0.5) {
+      //change the css background
+    } else {}
+
+
+  } else if (id === "Green") {
+    img.src = "img/cut-green-wire.png";
+
+  } else if (id === "third") {
+    console.log("you clicked on the 3rd div");
+  }
+}
+
+//
+//
+// fiveRandomNumbers();
+// console.log(blueNumber);
+//
+// function checkExplosion() {
+//   if (bluenumber > 1) {
+//     explode
+//   }
+// }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+//Reset Button
+// var resetButton = document.getElementById("reset");
+// resetButton.addEventListener('click', function() {
+//   location.reload();
+// });
